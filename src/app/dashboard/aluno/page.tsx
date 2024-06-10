484b84f5088a475072d52e6d2a575130e5ad7118
Leonardo <leonardorecco3@gmail.com>
2024-06-10 02:19:09 -0300

    feat: dash scroll

--- src/app/dashboard/aluno/page.tsx
+++ src/app/dashboard/aluno/page.tsx
@@ -1,11 +1,19 @@
 import { DashCarousel } from "@/components/ui/dash-carousel";
+import { DashScroll } from "@/components/ui/dash-scroll";
 import Sidebar from "@/components/ui/sidebar";
 
 export default function Aluno() {
     return (
     <div>
+        <div>
         <Sidebar/>
+        </div>
+        <div>
         <DashCarousel/>
+        </div>
+        <div>
+        <DashScroll/>
+        </div>
     </div>
     )
 }
---
+++ src/components/ui/dash-scroll.tsx
@@ -0,0 +1,53 @@
+import * as React from "react"
+import Image from "next/image"
+
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+
+export interface Artwork {
+  artist: string
+  art: string
+}
+
+export const works: Artwork[] = [
+  {
+    artist: "Ornella Binni",
+    art: "https://github.com/leozvx.png",
+  },
+  {
+    artist: "Tom Byrom",
+    art: "https://github.com/leozvx.png",
+  },
+  {
+    artist: "Vladimir Malyavko",
+    art: "https://github.com/leozvx.png",
+  },
+]
+
+export function DashScroll() {
+  return (
+    <ScrollArea className="w-96 whitespace-nowrap rounded-md border">
+      <div className="flex w-max space-x-4 p-4">
+        {works.map((artwork) => (
+          <figure key={artwork.artist} className="shrink-0">
+            <div className="overflow-hidden rounded-md">
+              <Image
+                src={artwork.art}
+                alt={`Photo by ${artwork.artist}`}
+                className="aspect-[3/4] h-fit w-fit object-cover"
+                width={300}
+                height={400}
+              />
+            </div>
+            <figcaption className="pt-2 text-xs text-muted-foreground">
+              Photo by{" "}
+              <span className="font-semibold text-foreground">
+                {artwork.artist}
+              </span>
+            </figcaption>
+          </figure>
+        ))}
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  )
+}
