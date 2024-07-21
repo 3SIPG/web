c43038c61ed3957925ca805c4b0ca3a08dcecf89
Leonardo Cardoso <leonardorecco3@gmail.com>
2024-07-21 20:19:31 -0300

    chore: test iframe

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,5 @@
+export default function Chatbot() {
+    return (
+        <iframe height="430" width="350" src="https://bot.dialogflow.com/eb27da05-4414-4894-9f52-c11cf90620f1"></iframe> 
+    )
+}
