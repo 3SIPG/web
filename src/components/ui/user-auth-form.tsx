94ce30941fa0f49c26515d9d4080f0e4bd0d9b37
Leonardo Cardoso <leonardorecco3@gmail.com>
2024-07-22 22:52:29 -0300

    feat: new "role" system

--- src/components/ui/user-auth-form.tsx
+++ src/components/ui/user-auth-form.tsx
@@ -6,43 +6,67 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-
+import { useRouter } from "next/navigation"
+import { useForm } from 'react-hook-form'
+import { useState } from "react"
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+interface Inputs {
+  user: string,
+  password: string
+}
+
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
-
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
-
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
+  const [isLoading, setLoading] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+  function onSubmit(data: Inputs) {
+    
+    if(data.user == "user") {
+      setLoading(true)
+      setTimeout(() => {
+        router.push("/dashboard/funcionario") 
+      }, 2000)
+    } else if (data.user == "admin") {
+      setLoading(true)
+      setTimeout(() => {
+        router.push("/dashboard/admin") 
+      }, 2000)
+    } else if (data.user == "creator") {
+      setLoading(true)
+      setTimeout(() => {
+        router.push("/dashboard/creator") 
+      }, 2000)
+    } else {
+      alert("This account doesn't exist!")
+    }
+       
   }
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-2">
           <div className="grid gap-[.5rem]">
-            <Label className="sr-only" htmlFor="email">
-              Email
+            <Label className="sr-only" htmlFor="user">
+              User
             </Label>
             <Input
-              id="email"
+             {...register("user")}
+              id="user"
               className=""
               placeholder="Usuário"
-              type="email"
+              type="user"
               autoCapitalize="none"
-              autoComplete="email"
+              autoComplete="user"
               autoCorrect="off"
               disabled={isLoading}
             />
-             <Label className="sr-only" htmlFor="email">
-              Email
+             <Label className="sr-only" htmlFor="user">
+              User
             </Label>
             <Input
+              {...register("password")}
               id="password"
               placeholder="Senha"
               type="password"
